0b7fc8a32a3ecf86a6a8b871b08e68a04bebce1d
EDiasAlberto <proxamon1@gmail.com>
2025-05-29 20:09:56 +0100

    feat: feedback box upon answering question

---
+++ web/app/components/question_elements/feedbackBox.jsx
@@ -0,0 +1,14 @@
+export default function FeedbackBox({ title, body }) {
+  return (
+    <div className="absolute bottom-0 left-0 w-full h-1/2 p-4 z-40">
+      <div className="bg-gray-800 text-white rounded-t-2xl p-6 shadow-2xl h-full overflow-y-auto">
+        <div className="text-lg font-semibold mb-2 text-teal-300">
+          {title}
+        </div>
+        <div className="text-sm whitespace-pre-line">
+          {body}
+        </div>
+      </div>
+    </div>
+  );
+}
