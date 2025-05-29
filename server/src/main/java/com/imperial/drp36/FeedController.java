0b6cf5ab12cd34bb6cace0df737830729b816bf4
sBubshait <s.bobshit@gmail.com>
2025-05-29 17:10:56 +0100

    feat: allow cros origin CORS for REST API

--- server/src/main/java/com/imperial/drp36/FeedController.java
+++ server/src/main/java/com/imperial/drp36/FeedController.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,6 +25,7 @@ public class FeedController {
   }
 
   @GetMapping("/getFeed")
+  @CrossOrigin(origins = "*")
   public ResponseEntity<FeedResponse> getFeed(@RequestParam(required = false) Long id) {
     if (id == null || id == 1) {
       FeedResponse response = new FeedResponse(
