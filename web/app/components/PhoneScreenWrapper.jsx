16d32c648e4d653686b6f5dcce0830b9f0621395
EDiasAlberto <proxamon1@gmail.com>
2025-05-29 15:25:29 +0100

    feat: componentify question header and set width restriction site-wide

---
+++ web/app/components/PhoneScreenWrapper.jsx
@@ -0,0 +1,9 @@
+export default function PageWrapper({ children }) {
+  return (
+    <div className="w-full min-h-screen flex justify-center bg-gray-100">
+      <div className="w-[390px] min-h-screen bg-white shadow-lg border border-gray-300">
+        {children}
+      </div>
+    </div>
+  );
+}
