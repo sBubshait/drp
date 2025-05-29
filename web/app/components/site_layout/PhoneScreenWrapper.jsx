241c9d61f25622f29c067444d1754784dd9b6ca0
EDiasAlberto <proxamon1@gmail.com>
2025-05-29 19:13:28 +0100

    fix: styling issue and hardcode question JSON:

--- web/app/components/site_layout/PhoneScreenWrapper.jsx
+++ web/app/components/site_layout/PhoneScreenWrapper.jsx
@@ -1,7 +1,7 @@
 export default function PageWrapper({ children }) {
   return (
     <div className="w-full min-h-screen flex justify-center bg-gray-100">
-      <div className="w-[390px] min-h-screen bg-white shadow-lg border border-gray-300">
+      <div className="w-[390px] min-h-screen bg-gray-200 shadow-lg border border-gray-300">
         {children}
       </div>
     </div>
