9818e01a1262b6b956e30b3be72a828dd8ef5a10
EDiasAlberto <proxamon1@gmail.com>
2025-05-29 20:09:56 +0100

    feat: feedback box upon answering question

--- web/app/components/site_layout/questionContent.jsx
+++ web/app/components/site_layout/questionContent.jsx
@@ -1,10 +1,13 @@
 import {useState} from 'react';
 import ContextBox from "../question_elements/contextBox.jsx";
 import MultipleChoiceQuestion from "../question_elements/multipleChoiceQuestion.jsx";
+import FeedbackBox from "../question_elements/feedbackBox.jsx";
 
 export default function QuestionContent({content}) {
 
   const [hasAnswered, setHasAnswered] = useState(false);
+  const [feedbackTitle, setFeedbackTitle] = useState("");
+  const [feedbackBody, setFeedbackBody] = useState("");
 
   if (!content) {
     return (
@@ -15,11 +18,20 @@ export default function QuestionContent({content}) {
   const {title, options, context, hasAnswer, answer, correctAnswerFeedback, wrongAnswerFeedback, generalAnswer} = content;
 
   function handleOptionClick(index) {
-    if(hasAnswer) {
-      (index===answer) ? console.log(correctAnswerFeedback) : console.log(wrongAnswerFeedback);
+    if (!hasAnswer) {
+      if (index === answer) {
+        setFeedbackTitle("Correct!");
+        setFeedbackBody(correctAnswerFeedback);
+      } else {
+        setFeedbackTitle("Not quite...");
+        setFeedbackBody(wrongAnswerFeedback);
+      }
     } else {
-      console.log("here is general feedback");
+      setFeedbackTitle("If you're curious...");
+      setFeedbackBody(generalAnswer);
     }
+
+    setHasAnswered(true); 
   }
 
   return (
@@ -33,6 +45,7 @@ export default function QuestionContent({content}) {
         options={options}
         onSelectOption={(index) => handleOptionClick(index)}
       />
+      {(hasAnswered && <FeedbackBox title={feedbackTitle} body={feedbackBody} />)}
     </>
   );
 }
