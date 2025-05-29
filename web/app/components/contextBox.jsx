be7c45968b17e5a1f466ae89a6d9cc096d2c4d03
EDiasAlberto <proxamon1@gmail.com>
2025-05-29 15:37:57 +0100

    feat: context box component

---
+++ web/app/components/contextBox.jsx
@@ -0,0 +1,10 @@
+export default function ContextBox({text}) {
+ return (
+  <div className="flex">
+    <div className="w-1 bg-gray-800 mr-4 flex-shrink-0"></div>
+      <div className="text-gray-700 leading-relaxed text-2xl">
+        {text}
+      </div>
+  </div>
+ ) 
+}
