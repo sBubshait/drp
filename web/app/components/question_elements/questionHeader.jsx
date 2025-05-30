bb1d0a600c0e1fffb942f152702b4408cd13d4b8
EDiasAlberto <proxamon1@gmail.com>
2025-05-30 01:56:22 +0100

    fix: question header counter bug

--- web/app/components/question_elements/questionHeader.jsx
+++ web/app/components/question_elements/questionHeader.jsx
@@ -2,7 +2,7 @@ export default function QuestionHeader({ questionNumber, totalQuestions, taskTyp
   return (
     <div className="flex w-[90%] bg-gray-800 text-white font-semibold text-3xl">
       <div className="px-4 py-3 border-r-4 border-gray-200">
-        {questionNumber} / {totalQuestions}
+        {questionNumber} / {2}
       </div>
       <div className="px-4 py-3">
         {taskType}
