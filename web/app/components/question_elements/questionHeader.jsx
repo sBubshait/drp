aebc89c3fb5345480d8b8fe158641c2edd9ce5dd
EDiasAlberto <proxamon1@gmail.com>
2025-06-04 20:58:39 +0100

    fix: question header for article summary does not show max

--- web/app/components/question_elements/questionHeader.jsx
+++ web/app/components/question_elements/questionHeader.jsx
@@ -1,8 +1,10 @@
 export default function QuestionHeader({ questionNumber, totalQuestions, taskType }) {
+  const isArticleSummary = taskType === "Article Summary";
+  
   return (
     <div className="flex w-[90%] bg-gray-800 text-white font-semibold text-3xl">
       <div className="px-4 py-3 border-r-4 border-gray-200">
-        {questionNumber} / {2}
+        {isArticleSummary ? questionNumber : `${questionNumber} / ${totalQuestions}`}
       </div>
       <div className="px-4 py-3">
         {taskType}
