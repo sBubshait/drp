fd742ded6bb8d0212279bc1f6eb95208a50c2956
sBubshait <s.bobshit@gmail.com>
2025-06-11 14:08:32 +0100

    feat: update users endpoints in server for consistency

--- server/src/main/java/com/imperial/drp36/UserController.java
+++ server/src/main/java/com/imperial/drp36/UserController.java
@@ -1,16 +1,10 @@
 package com.imperial.drp36;
 
 import com.imperial.drp36.entity.User;
-import com.imperial.drp36.model.DiscussionResponses;
-import com.imperial.drp36.model.StatusResponse;
-import com.imperial.drp36.model.UidResponse;
+import com.imperial.drp36.model.IdStatusResponse;
 import com.imperial.drp36.repository.UserRepository;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-import java.io.Console;
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,31 +16,30 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
 
   @Autowired
   private UserRepository userRepository;
 
-  @Tag(name = "Get UID")
-  @PostMapping("/generateUid")
-  public ResponseEntity<UidResponse> getUid() {
+  @Tag(name = "Users")
+  @PostMapping("/create")
+  public ResponseEntity<IdStatusResponse> getUid() {
     try {
       User user = new User(0);
       userRepository.save(user);
 
-      return ResponseEntity.ok(new UidResponse(200, "Successfully generated user id!", user.getId()));
-
+      return ResponseEntity.ok(new IdStatusResponse(200, "Successfully generated user id!", user.getId()));
     } catch (Exception e) {
       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
           .body(
-            new UidResponse(500, e.getMessage(), 0L)
+            new IdStatusResponse(500, e.getMessage(), 0L)
           );
     }
   }
 
-  @Tag(name = "Get user data")
-  @GetMapping("/")
+  @Tag(name = "Users")
+  @GetMapping("/get")
   public ResponseEntity<User> getUser(@RequestParam Long id) {
     try {
       Optional<User> user = userRepository.findById(id);
