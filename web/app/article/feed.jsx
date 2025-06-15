a4bcfb7ac77f85105a57bad28cbf4e5a2bcdb511
Themis Demetriades <themis100@outlook.com>
2025-06-15 16:17:42 +0100

    fix: fix hydration and other DOM issues

--- web/app/article/feed.jsx
+++ web/app/article/feed.jsx
@@ -1,37 +1,42 @@
 import { useRef, useEffect, useState } from "react";
-import { getCoords, getId, getNotId, resetScroll } from "./feedScrollController";
+import { getCoords, getId, getNotId } from "./feedScrollController";
 import "./feed.css";
 
 export default function Feed() {
 
   // If you see a brief 'ghost image' of a future page, turn this number higher
-  const TP_TIMEOUT = 300;
+  const TP_TIMEOUT = 100;
 
   const [offset, setOffset] = useState({x: 0, y: 0});
   const [specialPage, setSpecialPage] = useState({id: null, element: null});
+
   const fetchedPages = useRef(new Map())
 
   // Manage feed content
   const genFeedContent = (virtualCoords) => {
-   return <div className="block"> {`x: ${virtualCoords.x} y: ${virtualCoords.y}`} </div>
+   return <div className=""> {`x: ${virtualCoords.x} y: ${virtualCoords.y}`} </div>
   }
 
   const feedContent =
     getCoords(offset).filter((coords) => coords.virtualCoords).reduce((content, coords) => {
       
       const id = getId(coords);
+      const notId = getNotId(coords);
 
       const cache = fetchedPages.current.get(id);
       const data = cache ? cache : genFeedContent(coords.virtualCoords);
       fetchedPages.current.set(id, data);
 
       const element =
-          <div key={id} id={getNotId(coords)} className={`col-start-${coords.trueCoords.x + 1} row-start-${coords.trueCoords.y + 1}`}> 
+          <div key={id} id={notId} className={`articleBlock col-start-${coords.trueCoords.x + 1} row-start-${coords.trueCoords.y + 1}`}> 
             {data}
           </div>
 
-      if (id == specialPage.id) {
-          setTimeout(() => setSpecialPage({...specialPage, element: element}), TP_TIMEOUT);
+      if (notId == specialPage.notId) {
+          setTimeout(() => {
+            setSpecialPage({...specialPage, id: null, notId: null, element: element});
+            //setRecenter(false);
+          }, TP_TIMEOUT);
       } else {
           content.set(id, element);
     }
@@ -41,9 +46,9 @@ export default function Feed() {
 
   // On load
   useEffect(() => {
-    try {
-      document.getElementById("4-4").scrollIntoView();
-    } catch (e) {}
+    const container = document.getElementById("grid");
+    container.scrollLeft = container.offsetWidth * 4;
+    container.scrollTop = container.offsetHeight * 4;
   })
 
   return(
@@ -55,14 +60,23 @@ export default function Feed() {
         const row = Math.round(container.scrollTop / container.offsetHeight);
 
         if (col != 4 || row != 4) {
-          setOffset({x: offset.x + col - 4, y: offset.y + row - 4});
-          setSpecialPage({id: getId()});
+          const offX = offset.x + col - 4;
+          const offY = offset.y + row - 4;
+          const id = `${offX}-${offY}`;
+          const notId = `${col}-${row}`;
+
+          setOffset({x: offX, y: offY});
+          setSpecialPage({id: id, notId: notId, element: 
+              <div key={id} id={notId} className={`articleBlock col-start-${col} row-start-${row}`}> 
+                {fetchedPages.current.get(id)}
+              </div>
+            });
           //updateBlocks();
         }
       }, 80); // debounce
     }}>
+      {specialPage.element}
       {[...feedContent.values()]}
-      <specialPage />
     </div>
   );
 }
